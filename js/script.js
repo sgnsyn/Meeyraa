512867fff087d8f0b9c0e260ae7d688cbe25e417
sagni <sgnsyn45@gmail.com>
2025-02-27 13:03:02 +0300

    Responsiveness fixes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const openBtn = document.querySelector(".open-btn");
 const closeBtn = document.querySelector(".close-btn");
 const bName = document.querySelector(".hero-brand-desktop");
+const mainSection = document.querySelector(".main-section");
 
 function openMobileMenu() {
   const menuContainer = document.querySelector(".menu-container");
@@ -14,5 +15,10 @@ function closeMobileMenu() {
 window.addEventListener("load", () => {
   bName.classList.add("active");
 });
+window.addEventListener("resize", () => {
+  mainSection.style.height = window.innerHeight;
+});
+mainSection.style.height = window.innerHeight;
+
 openBtn?.addEventListener("click", openMobileMenu);
 closeBtn?.addEventListener("click", closeMobileMenu);
